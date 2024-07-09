6d04c5acbf88d69587210cce160f9913f2aa2b47
sahelzaff <sahelzaffar2@gmail.com>
2024-07-09 23:42:55 +0530

    chnages made

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ import { connectDB } from './config/db.js';
 
 // Import routes
 import formRoutes from './routes/formRoutes.js';
-import leadsRoutes from './routes/leadsRoutes.js'; 
+import leadsRoutes from './routes/leadsRoutes.js';
 import blogRoutes from './routes/blogRoutes.js';
 import googleRoutes from './routes/googleRoutes.js';
-import emailRoutes from './routes/emailRoutes.js'; 
+import emailRoutes from './routes/emailRoutes.js';
 
 // App config
 dotenv.config();
@@ -21,14 +21,14 @@ const port = process.env.PORT || 4000;
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static(path.join(path.resolve(), 'uploads'))); 
+app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
 
 // DB connection
 connectDB();
 
 // Routes
 app.use('/api/form', formRoutes);
-app.use('/api/lead-cal', leadsRoutes); 
+app.use('/api/lead-cal', leadsRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/google', googleRoutes);
 app.use('/api/email', emailRoutes);
